521c71e7c39893ded086c880393e39c4f9161442
Feihong Hsu <feihong.hsu@gmail.com>
2016-05-28 16:10:21 -0500

    Update install_node.sh

--- ubuntu/install_node.sh
+++ ubuntu/install_node.sh
@@ -2,7 +2,7 @@
 
 cd ~/Downloads
 
-wget -O nodejs.tar.xz https://nodejs.org/dist/v6.1.0/node-v6.1.0-linux-x64.tar.xz
+wget -O nodejs.tar.xz https://nodejs.org/dist/v6.2.0/node-v6.2.0-linux-x64.tar.xz
 sudo tar -C /usr/local --strip-components 1 -xJf nodejs.tar.xz
 
 # https://github.com/creationix/nvm
