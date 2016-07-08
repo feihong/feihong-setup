c272b1ee8919d419fa6248488772c7f9b4c51f77
Feihong <feihong.hsu@gmail.com>
2016-07-08 16:24:22 -0500

    Add python3-dev to list of setup packages

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,3 +1,4 @@
-sudo apt install -y python-pip build-essential python-dev libffi-dev libssl-dev
+sudo apt install -y python-pip build-essential python-dev libffi-dev \
+  libssl-dev python3-dev
 sudo pip install ansible markupsafe
 ansible-playbook --ask-become-pass setup.yml
