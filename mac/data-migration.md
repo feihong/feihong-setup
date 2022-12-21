ae83ce266cbcf21cb841241219e1eb39f784d95a
Feihong Hsu <feihong.hsu@gmail.com>
2022-12-20 18:54:31 -0600

    Improve migrate data script

--- mac/data-migration.md
+++ mac/data-migration.md
@@ -7,11 +7,21 @@ On target machine, allow SSH login from another machine
 - System Settings > General > Sharing
 - Check Remote Login
 - Click (i) next to Remote Login to see the ssh command which contains the IP address
+- System Settings > Displays > Advanced...
+- Check `Prevent automatic sleeping on power adapter when the display is off`
 
 On source machine
 
 - Create `secrets.py` and fill in the target machine's IP address
 - Run `python migrate_data.py`
+
+On target machine
+
+- System Settings > General > Sharing
+- Uncheck Remote Login
+- Click (i) next to Remote Login to see the ssh command which contains the IP address
+- System Settings > Displays > Advanced...
+- Uncheck `Prevent automatic sleeping on power adapter when the display is off`
 
 ## Backup music metadata
 
--- mac/migrate_data.py
+++ mac/migrate_data.py
@@ -1,17 +1,19 @@
 # https://unix.stackexchange.com/questions/174674/rsync-a-list-of-directories-with-absolute-path-in-text-file
 import subprocess
 from pathlib import Path
-from secrets import server
+from settings import server
 
 folders = """
-Books
-Comics
+# Books
+# Comics
 Documents
 Games
 Music/tracks
 Setup
 Videos
-""".strip().splitlines()
+"""
+folders = [f for f in folders.strip().splitlines() if not f.startswith('#')]
+
 
 home_dir = Path('~').expanduser()
 user = str(home_dir).split('/')[-1]
@@ -20,9 +22,9 @@ source_directories = [str(home_dir / folder) for folder in folders]
 
 cmd = [
   'rsync',
-  '--dry-run', # comment this out when you want to move the files over for real
-  '-arR', 
-  '--numeric-ids', 
+  # '--dry-run', # comment this out when you want to move the files over for real
+  '-a', # archive mode; equals -rlptgoD
+  '--numeric-ids', # don't map uid/gid values by user/group name
   '--progress',
   '--exclude=.DS_Store',
 ] + source_directories + [
