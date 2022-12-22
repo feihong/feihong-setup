e6b4eb6d5c3a585c41afdf1efca18933d214b6ac
Feihong Hsu <feihong.hsu@gmail.com>
2022-12-22 13:37:13 -0600

    Can't get duplicateTo_ method to work

--- mac/restore_playlists.py
+++ mac/restore_playlists.py
@@ -9,22 +9,45 @@ downloads_dir = Path('~/Downloads').expanduser()
 playlist_files = [f for f in downloads_dir.glob('*.json') if f.name != 'tracks.json']
 
 app = Foundation.SBApplication.applicationWithBundleIdentifier_("com.apple.Music")
+tracks = app.sources()[0].tracks()
 
 def create_playlist(name):
   props = {'name': name}
   playlist = app.classForScriptingClass_("playlist").alloc().initWithProperties_(props)
-  app.sources()[0].playlists().insertObject_atIndex_(playlist, 0)
+  return app.sources()[0].playlists().addObject_(playlist)
 
-# for track in playlist:
-#   print(track.keys())
-#   break
+def get_track(location):
+  for track in tracks:
+    if track.location().path() == location:
+      return track
+  
+  return None
+
+# tracks = app.sources()[0].tracks()
+# for track in tracks:
+#   print(track.location().path())
 
 for playlist_file in playlist_files:
   print(playlist_file)
+  name = playlist_file.stem
   locations = [item['location'] for item in json.loads(playlist_file.read_bytes())]
-  for loc in locations:
-    if not os.path.exists(loc):
-      print(f'{loc} does not exist')
+  for location in locations:
+    if not os.path.exists(location):
+      print(f'{location} does not exist')
       sys.exit(1)
 
-  create_playlist(playlist_file.stem)
+  playlist = create_playlist(name)
+
+  for i, track in enumerate(app.sources()[0].tracks()):
+    print(track.name())
+    # not working
+    track.duplicateTo_(playlist)
+    if i > 10:
+      break
+
+  # for location in locations:
+  #   print(f'Adding {track.name()} to {name}')
+  #   track = get_track(location)
+  #   if track is not None:
+  #     track.duplicateTo_(playlist)
+      
