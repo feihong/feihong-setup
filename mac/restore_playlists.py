e87b734c007990299e08ff5bf2ba007595328bbf
Feihong Hsu <feihong.hsu@gmail.com>
2022-12-22 14:25:03 -0600

    Finally got restore_playlists Python script to work!

--- mac/restore_playlists.py
+++ mac/restore_playlists.py
@@ -3,51 +3,57 @@ import os.path
 import json
 from pathlib import Path
 import Foundation
-import ScriptingBridge
+import ScriptingBridge as _ # even if you don't use it, you must still import it
 
 downloads_dir = Path('~/Downloads').expanduser()
 playlist_files = [f for f in downloads_dir.glob('*.json') if f.name != 'tracks.json']
 
 app = Foundation.SBApplication.applicationWithBundleIdentifier_("com.apple.Music")
 tracks = app.sources()[0].tracks()
+playlists = app.sources()[0].playlists()
 
 def create_playlist(name):
   props = {'name': name}
   playlist = app.classForScriptingClass_("playlist").alloc().initWithProperties_(props)
-  return app.sources()[0].playlists().addObject_(playlist)
+  playlists.addObject_(playlist)
+  return playlist
 
-def get_track(location):
+def delete_playlist(name):
+  for i, playlist in enumerate(playlists):
+    if playlist.name() == name:
+      playlists.removeObjectAtIndex_(i)
+      return
+
+def create_tracks_map():
+  print('Building tracks map, might take a long time...')
+  result = {}
   for track in tracks:
-    if track.location().path() == location:
-      return track
-  
-  return None
+    # Must explicitly convert to string, otherwise the type is objc.pyobjc_unicode
+    key = str(track.location().path())
+    result[key] = track
+  return result
 
-# tracks = app.sources()[0].tracks()
-# for track in tracks:
-#   print(track.location().path())
+def main():
+  tracks_map = create_tracks_map()
 
-for playlist_file in playlist_files:
-  print(playlist_file)
-  name = playlist_file.stem
-  locations = [item['location'] for item in json.loads(playlist_file.read_bytes())]
-  for location in locations:
-    if not os.path.exists(location):
-      print(f'{location} does not exist')
-      sys.exit(1)
+  for playlist_file in playlist_files:
+    print(playlist_file)
+    name = playlist_file.stem
+    locations = [item['location'] for item in json.loads(playlist_file.read_bytes())]
+    # Verify that all files actually exist:
+    for location in locations:
+      if not os.path.exists(location):
+        print(f'{location} does not exist')
+        sys.exit(1)
 
-  playlist = create_playlist(name)
+    # delete_playlist(name)
+    playlist = create_playlist(name)
 
-  for i, track in enumerate(app.sources()[0].tracks()):
-    print(track.name())
-    # not working
-    track.duplicateTo_(playlist)
-    if i > 10:
-      break
+    # Add all tracks to the new playlist
+    for location in locations:
+      track = tracks_map[location]
+      track.duplicateTo_(playlist)
+      print(f'Added {track.name()} to {name}')
 
-  # for location in locations:
-  #   print(f'Adding {track.name()} to {name}')
-  #   track = get_track(location)
-  #   if track is not None:
-  #     track.duplicateTo_(playlist)
-      
+if __name__ == '__main__':
+  main()
