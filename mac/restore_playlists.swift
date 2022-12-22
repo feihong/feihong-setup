ae65a73f3bf98488857b821b8c6ba696b3ae393b
Feihong Hsu <feihong.hsu@gmail.com>
2022-12-22 00:13:36 -0600

    Add function to get Library

--- mac/restore_playlists.swift
+++ mac/restore_playlists.swift
@@ -15,24 +15,54 @@ func getPlaylistFiles() -> [URL] {
   return directoryContents.filter { $0.typeIdentifier == "public.json"  && $0.lastPathComponent != "tracks.json" }
 }
 
+func makeTracksMap(_ tracks: SBElementArray) -> [String: MusicFileTrack] {
+  var result: [String: MusicFileTrack] = [:]
+  for case let track as MusicFileTrack in tracks {
+    let key = track.location!.path
+    result[key] = track
+  }
+  return result
+} 
+
 guard let app: MusicApplication = SBApplication(bundleIdentifier: "com.apple.Music") else {
   print("Could not load Music app")
   exit(1)
 }
 
-func getMusicPlaylist() -> MusicPlaylist? {
-  for case let playlist as MusicPlaylist in app.playlists!() {
-    if playlist.name! == "Music" {
-      return playlist
-    }  
+guard let library = getLibrary() else {
+  print("Could not access Library")
+  exit(1)
+}
+
+func getLibrary() -> MusicSource? {
+  for case let source as MusicSource in app.sources!() {
+    if source.name! == "Library" {
+      return source
+    }
   }
   return nil
 }
 
+func addPlaylist(name: String) {
+  // totally stuck here
+  // let thing = app.class("forScriptingClass:")
+}
+
+
+// func getMusicPlaylist() -> MusicPlaylist? {
+//   for case let playlist as MusicPlaylist in app.playlists!() {
+//     if playlist.name! == "Music" {
+//       return playlist
+//     }  
+//   }
+//   return nil
+// }
+
 // let playlist = getMusicPlaylist()!
 // let tracks = playlist.tracks!()
 
+// let tracksMap = makeTracksMap(tracks)
+
 for p in getPlaylistFiles() {
-  // print(p.typeIdentifier!)
-  print(p.lastPathComponent)
+  addPlaylist(name: p.lastPathComponent)
 }
